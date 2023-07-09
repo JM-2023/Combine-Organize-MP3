dec4ed6c3731d66d62b0463f976cf1044b78afb1
JM-2023 <github@jiminli.me>
2023-07-10 07:33:23 +0800

    Update combine.py
    Process files concurrently

--- combine.py
+++ combine.py
@@ -2,6 +2,7 @@ from moviepy.editor import concatenate_audioclips, AudioFileClip
 import os
 from datetime import datetime
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor
 
 directory = os.getcwd()  # Current directory
 
@@ -15,13 +16,15 @@ for mp3_file in mp3_files:
     base_name = os.path.splitext(mp3_file)[0]  # Removes .mp3
     parts = base_name.split(" ")
 
+    if len(parts) < 2:
+        continue  # Skip files that don't have the expected format
+
     date_string = parts[1]
     date = datetime.strptime(date_string, "%Y-%m-%d").date()
 
     grouped_files[date].append(mp3_file)
 
-# For each group, sort files by time and concatenate
-for date, files in grouped_files.items():
+def process_files(date, files):
     # Sort the list based on time in the filename
     files.sort(key=lambda x: datetime.strptime(os.path.splitext(x.split(" ")[2])[0], "%H-%M-%S-%f"))
 
@@ -29,3 +32,10 @@ for date, files in grouped_files.items():
 
     final_clip = concatenate_audioclips(audio_clips)
     final_clip.write_audiofile(os.path.join(directory, f"{date.strftime('%Y%m%d')}.mp3"))
+
+# Process files concurrently
+with ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_files, date, files) for date, files in grouped_files.items()]
+    # Wait for all tasks to complete
+    for future in futures:
+        future.result()
