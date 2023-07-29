298c652c98e8ad96ea85e4f01a243630f08c902d
JM-2023 <github@jiminli.me>
2023-07-29 10:43:41 +0800

    Update combine.py
    new filename format

--- combine.py
+++ combine.py
@@ -24,6 +24,34 @@ for mp3_file in mp3_files:
 
     grouped_files[date].append(mp3_file)
 
+def parse_time_from_filename(filename):
+    time_str = os.path.splitext(filename.split(" ")[1])[0]
+    try:
+        # Try to parse time with milliseconds
+        return datetime.strptime(time_str, "%H-%M-%S-%f")
+    except ValueError:
+        try:
+            # If it fails, parse time without milliseconds
+            return datetime.strptime(time_str, "%H-%M-%S")
+        except ValueError:
+            # If it fails again, parse time without seconds
+            return datetime.strptime(time_str, "%H-%M")
+
+def parse_date_and_time_from_filename(filename):
+    base_name = os.path.splitext(filename)[0]  # Removes .mp3
+    date_str, time_str = base_name.split(" ")
+
+    date = datetime.strptime(date_str, "%Y-%m-%d").date()
+
+    try:
+        # Try to parse time with milliseconds
+        time = datetime.strptime(time_str, "%H-%M-%S-%f").time()
+    except ValueError:
+        # If it fails, parse time without milliseconds
+        time = datetime.strptime(time_str, "%H-%M-%S").time()
+
+    return date, time
+
 def process_files(date, files):
     # Sort the list based on time in the filename
     files.sort(key=lambda x: parse_time_from_filename(x))
@@ -38,22 +66,21 @@ def process_files(date, files):
         else:
             print(f"Skipping zero-duration clip: {mp3_file}")
 
+    if not audio_clips:
+        print(f"No valid audio clips found for {date}")
+        return
+
+    # Get the first audio clip filename
+    first_clip_filename = files[0]
+
+    # Extract date and time from the first audio clip filename
+    first_clip_date, first_clip_time = parse_date_and_time_from_filename(first_clip_filename)
+
+    # Format the date and time for the output filename
+    output_filename = f"{first_clip_date.strftime('%Y%m%d')} {first_clip_time.strftime('%H-%M')}.mp3"
+
     final_clip = concatenate_audioclips(audio_clips)
-    final_clip.write_audiofile(os.path.join(directory, f"{date.strftime('%Y%m%d')}.mp3"))
-
-def parse_time_from_filename(filename):
-    time_str = os.path.splitext(filename.split(" ")[1])[0]
-    try:
-        # Try to parse time with milliseconds
-        return datetime.strptime(time_str, "%H-%M-%S-%f")
-    except ValueError:
-        try:
-            # If it fails, parse time without milliseconds
-            return datetime.strptime(time_str, "%H-%M-%S")
-        except ValueError:
-            # If it fails again, parse time without seconds
-            return datetime.strptime(time_str, "%H-%M")
-
+    final_clip.write_audiofile(os.path.join(directory, output_filename))
 
 # Process files concurrently
 with ThreadPoolExecutor() as executor:
