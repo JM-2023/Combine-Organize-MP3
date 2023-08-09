754631034eeb5e2db002e51a575ddbe4ecba7766
JM-2023 <github@jiminli.me>
2023-08-09 09:32:14 +0800

    Update combine.py
    Using regular expressions to extract the date and time from the filename, regardless of their position in the string.

--- combine.py
+++ combine.py
@@ -3,54 +3,54 @@ import os
 from datetime import datetime
 from collections import defaultdict
 from concurrent.futures import ThreadPoolExecutor
+import re
 
 directory = os.getcwd()  # Current directory
 
 # List comprehension to get all mp3 files
 mp3_files = [f for f in os.listdir(directory) if f.endswith('.mp3')]
 
+def parse_time_from_filename(filename):
+    date_time_pattern = re.compile(r'(\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}(-\d{2})?)')
+    match = date_time_pattern.search(filename)
+    if match:
+        date_str, time_str = match.groups()[:2]  # Use the first two groups, which include the date and the optional seconds
+        date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        try:
+            # Try to parse time with seconds
+            time = datetime.strptime(time_str, "%H-%M-%S").time()
+        except ValueError:
+            # If it fails, parse time without seconds
+            time = datetime.strptime(time_str, "%H-%M").time()
+
+        return datetime.combine(date, time)
+    else:
+        return None
+
+def parse_date_and_time_from_filename(filename):
+    date_time_pattern = re.compile(r'(\d{4}-\d{2}-\d{2}) (\d{2}-\d{2}(-\d{2})?)')
+    match = date_time_pattern.search(filename)
+    if match:
+        date_str, time_str = match.groups()[:2]  # Use the first two groups, which include the date and the optional seconds
+        date = datetime.strptime(date_str, "%Y-%m-%d").date()
+        try:
+            # Try to parse time with seconds
+            time = datetime.strptime(time_str, "%H-%M-%S").time()
+        except ValueError:
+            # If it fails, parse time without seconds
+            time = datetime.strptime(time_str, "%H-%M").time()
+
+        return date, time
+    else:
+        return None, None
+
 # Group files by date
 grouped_files = defaultdict(list)
 for mp3_file in mp3_files:
     # Extract date and time from filename
-    base_name = os.path.splitext(mp3_file)[0]  # Removes .mp3
-    parts = base_name.split(" ")
-
-    if len(parts) < 2:
-        continue  # Skip files that don't have the expected format
-
-    date_string = parts[0]
-    date = datetime.strptime(date_string, "%Y-%m-%d").date()
-
-    grouped_files[date].append(mp3_file)
-
-def parse_time_from_filename(filename):
-    time_str = os.path.splitext(filename.split(" ")[1])[0]
-    try:
-        # Try to parse time with milliseconds
-        return datetime.strptime(time_str, "%H-%M-%S-%f")
-    except ValueError:
-        try:
-            # If it fails, parse time without milliseconds
-            return datetime.strptime(time_str, "%H-%M-%S")
-        except ValueError:
-            # If it fails again, parse time without seconds
-            return datetime.strptime(time_str, "%H-%M")
-
-def parse_date_and_time_from_filename(filename):
-    base_name = os.path.splitext(filename)[0]  # Removes .mp3
-    date_str, time_str = base_name.split(" ")
-
-    date = datetime.strptime(date_str, "%Y-%m-%d").date()
-
-    try:
-        # Try to parse time with milliseconds
-        time = datetime.strptime(time_str, "%H-%M-%S-%f").time()
-    except ValueError:
-        # If it fails, parse time without milliseconds
-        time = datetime.strptime(time_str, "%H-%M-%S").time()
-
-    return date, time
+    date, _ = parse_date_and_time_from_filename(mp3_file)
+    if date is not None:
+        grouped_files[date].append(mp3_file)
 
 def process_files(date, files):
     # Sort the list based on time in the filename
@@ -75,6 +75,10 @@ def process_files(date, files):
 
     # Extract date and time from the first audio clip filename
     first_clip_date, first_clip_time = parse_date_and_time_from_filename(first_clip_filename)
+
+    if first_clip_date is None or first_clip_time is None:
+        print(f"Could not parse date and time from filename: {first_clip_filename}")
+        return
 
     # Format the date and time for the output filename
     output_filename = f"{first_clip_date.strftime('%Y%m%d')} {first_clip_time.strftime('%H-%M')}.mp3"
