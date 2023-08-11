d3422dab0a619f8b4a32564aed29339ef395f633
Li <136283672+JM-2023@users.noreply.github.com>
2023-08-12 05:29:28 +0800

    Update organize.py
    better detail file name

--- organize.py
+++ organize.py
@@ -1,17 +1,34 @@
 import os
 import shutil
 import re
+from collections import defaultdict
 
-# Get all mp3 files in current directory
+# Regular expression to find the date and time
+date_time_pattern = re.compile(r'(\d{4})-(\d{2})-(\d{2}) (\d{2}-\d{2})')
+
+# Create a dictionary to store files by date
+files_by_date = defaultdict(list)
+
+# Get all mp3 files in the current directory
 mp3_files = [f for f in os.listdir() if f.endswith('.mp3')]
 
-# Regular expression to find the date
-date_pattern = re.compile(r'\d{4}-\d{2}-\d{2}')
-
+# Extract date and time and organize files by date
 for file in mp3_files:
-    match = date_pattern.search(file)
+    match = date_time_pattern.search(file)
     if match:
-        date = match.group()
-        if not os.path.exists(date):
-            os.makedirs(date)
-        shutil.move(file, date)
+        year, month, day, time = match.groups()
+        # Formatting the date as YYYYMMDD
+        formatted_date = f"{year}{month}{day}"
+        files_by_date[formatted_date].append((time, file))
+
+# Sort files by date and time, and move to corresponding folders
+for date, files in files_by_date.items():
+    # Sort files by time
+    files.sort()
+    # Create folder with the earliest file time
+    folder_name = f"{date} {files[0][0]}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+    # Move files to the created folder
+    for _, file in files:
+        shutil.move(file, folder_name)
